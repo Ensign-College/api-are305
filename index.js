12f2bc22e77cfa0f9fa38adf6598049d8d568112
are305 <132591668+are305@users.noreply.github.com>
2024-01-10 17:23:32 +0000

    1/10 Added express, and devcontainer

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express');//Express makes API to connect FrontEnd with Database
+
+//import express from 'express'; <- modern way
+
+const app = new express(); //Create an Express application
+
+app.listen(3000);//Listen for web requests from the FrontEnd and don't stop
+
+const boxes = [
+    {boxId:1},
+    {boxId:2},
+    {boxId:3},
+    {boxId:4}
+];
+
+// 1-URL
+// 2-a function to return boxes
+// req= the request from the browser
+// res= the response from the browser
+app.get('/boxes',(req,res)=>{
+    //Send the boxes to the browser
+    res.send(JSON.stringify(boxes));//Convert boxes to a string
+});//Return boxes to the user
+
 console.log('Hello');
